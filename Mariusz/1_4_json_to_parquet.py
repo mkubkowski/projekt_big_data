adf7049275860caa8d0b04c5a8b6be33611fa79c
mkubkowski <49030627+mkubkowski@users.noreply.github.com>
2024-01-17 18:40:02 +0100

    added 1.4 and 1.5

---
+++ Mariusz/1_4_json_to_parquet.py
@@ -0,0 +1,31 @@
+# Przykładowa implementacja
+import sys
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.dynamicframe import DynamicFrame
+from awsglue.job import Job
+
+args = getResolvedOptions(sys.argv, ["JOB_NAME"])
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+job.init(args["JOB_NAME"], args)
+
+dataFrame = spark.read.json("s3://bucket-raw-data16012024/part-01.json")
+
+dynamicFrame = DynamicFrame.fromDF(dataFrame, glueContext, "")
+
+
+glueContext.write_dynamic_frame.from_options(
+    frame=dynamicFrame,
+    connection_type='s3',
+    connection_options={
+        'path': "s3://bucket-formatted-data-17012024", "partitionKeys": ["review_date"]
+    },
+    format='parquet',
+)
+
+job.commit()
